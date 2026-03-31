d6a76f0a1cfa272370a25121a56708223da417cc
Nemmen <aryankammboz@gmail.com>
2026-03-31 17:40:54 +0530

    ghcxvhgh

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "**.theuniques.in",
+      },
     ],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
